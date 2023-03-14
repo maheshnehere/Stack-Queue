850b96ee2c4ced950a2668ac015fd3fb27e16636
maheshnehere <maheshnehere93@gmail.com>
2023-03-14 13:16:55 +0530

    Added:Welcome program

---
+++ src/Bridgelabz/StackandQueue.java
@@ -0,0 +1,11 @@
+package Bridgelabz;
+
+/**
+ * Welcome
+ */
+public class StackandQueue {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack & Queue Program");
+    }
+}
